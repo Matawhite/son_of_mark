1fced4d3f1cedcb94ae2f9442e35da928460937c
Jack Walker <jackw2050@gmail.com>
2016-10-06 19:35:32 -0500

    added modal for join room.  Added normal color picker js file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
+var moment = require('moment');
 var port     = process.env.PORT || 8080;
 var path = require('path');
 var passport = require('passport');
@@ -43,8 +44,48 @@ var io = socketIo.listen(server);
 
 var line_history = [];
 var chat_history = [];
+var socketId = "";
+
+
+// Sends current users to provided socket
+function sendCurrentUsers(socket) {
+    console.log("send users");
+    var info = clientInfo[socket.id];
+    var users = [];
+
+    if (typeof info === 'undefined') {
+        return;
+    }
+
+    Object.keys(clientInfo).forEach(function(socketId) {
+        var userInfo = clientInfo[socketId];
+
+        if (info.room === userInfo.room) {
+            users.push(userInfo.name);
+        }
+    });
+
+    socket.emit('message', {
+        name: 'System',
+        text: 'Current users: ' + users.join(', '),
+        timestamp: moment().valueOf()
+    });
+}
+
 
 io.on('connection', function(socket) {
+
+// Session started message
+    socket.emit('message', {
+        name: '',
+        text: 'Session started',
+        timestamp: moment().valueOf()
+    });
+
+
+
+
+
     for (var i in line_history) {
         socket.emit('draw_line', line_history[i]);
     }
@@ -52,6 +93,9 @@ io.on('connection', function(socket) {
     	var lineObject = {line: data.line, color: data.color, width: data.width};
         line_history.push(lineObject);
         io.emit('draw_line', lineObject);
+        // This is version to emit to a specific room
+        //io.to(data.room).emit('draw_line', { line: data.line });
+
     });
 
     socket.on('clearCanvas', function() {
@@ -62,10 +106,69 @@ io.on('connection', function(socket) {
     for (var i in chat_history) {
         socket.emit('chatMessage', chat_history[i]);
     }
+
+    // old version
     socket.on('chatMessage', function(msg) {
     	chat_history.push(msg);
     	io.emit('chatMessage', msg);
     });
+
+
+    socket.on('message', function(message) {
+        console.log('Message received: ' + message.text);
+        if (message.text === '@currentUsers') {
+            sendCurrentUsers(socket);
+        } else {
+            message.timestamp = moment().valueOf();
+            io.to(clientInfo[socket.id].room).emit('message', message);
+        }
+    });
+
+
+
+
+
+    socket.on('disconnect', function() {
+        var userData = clientInfo[socket.id];
+        console.log("disconnect");
+        if (typeof userData !== 'undefined') {
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message', {
+                name: 'System',
+                text: userData.name + ' has left the room.',
+                timestamp: moment().valueOf()
+            });
+            delete clientInfo[socket.id];
+        }
+    });
+
+    socket.on('joinRoom', function(req) {
+        clientInfo[socket.id] = req;
+        socketId = req.room;
+        socket.join(req.room);
+
+
+        // console.log(clientInfo[socket.id]);
+        room = clientInfo[socket.id].room;
+        console.log("joining room " + room);
+        for (var i in line_history) {
+            // console.log(line_history);
+            io.to(req.room).emit('draw_line', { line: line_history[i] });
+            //socket.broadcast.to(room).emit('draw_line', { line: line_history[i] });
+            // socket.broadcast.to(socketId).emit('draw_line', { line: line_history[i] });
+        }
+
+        socket.broadcast.to(req.room).emit('message', {
+            name: 'System',
+            text: req.name + ' has entered the room.',
+            timestamp: moment().valueOf()
+        });
+    });
+
+
+
+
+
 });
 
 // launch ======================================================================
