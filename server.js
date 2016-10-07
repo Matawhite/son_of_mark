050aba07dd0ac47c7029d4b37745674a5baa555a
Jack Walker <jackw2050@gmail.com>
2016-10-06 20:34:57 -0500

    initial

--- server.js
+++ server.js
@@ -128,19 +128,19 @@ io.on('connection', function(socket) {
 
 
 
-    socket.on('disconnect', function() {
-        var userData = clientInfo[socket.id];
-        console.log("disconnect");
-        if (typeof userData !== 'undefined') {
-            socket.leave(userData.room);
-            io.to(userData.room).emit('message', {
-                name: 'System',
-                text: userData.name + ' has left the room.',
-                timestamp: moment().valueOf()
-            });
-            delete clientInfo[socket.id];
-        }
-    });
+    // socket.on('disconnect', function() {
+    //     var userData = clientInfo[socket.id];
+    //     console.log("disconnect");
+    //     if (typeof userData !== 'undefined') {
+    //         socket.leave(userData.room);
+    //         io.to(userData.room).emit('message', {
+    //             name: 'System',
+    //             text: userData.name + ' has left the room.',
+    //             timestamp: moment().valueOf()
+    //         });
+    //         delete clientInfo[socket.id];
+    //     }
+    // });
 
     socket.on('joinRoom', function(req) {
         clientInfo[socket.id] = req;
